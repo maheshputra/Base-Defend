e1087fcab887943f84542677011984b601c7017e
Kurou23 <32098313+Kurou23@users.noreply.github.com>
2018-03-21 02:11:59 +0700

    Add Tower Idle

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+    public bool idle;
+    private float speed = 15f;
+	// Use this for initialization
+	void Start () {
+        idle = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (idle) {
+			transform.Rotate(0,0,speed*Time.deltaTime);
+        }
+	}
+}
