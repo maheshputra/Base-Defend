d09892d8247d6eeb277e70e9b9b2720c072280a6
Kurou23 <32098313+Kurou23@users.noreply.github.com>
2018-03-21 23:29:34 +0700

    New Commit

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour {
+	public Transform enemy;
+
+	public Transform SpawnPoint;
+
+	public float timeBetweenWaves = 5f;
+	private float timeCountDown = 2f;
+
+	private int waveNumber = 0;
+	
+	// Update is called once per frame
+	void Update () {
+		if (timeCountDown <= 0f) {
+			StartCoroutine (SpawnWave());
+			timeCountDown = timeBetweenWaves;
+		}
+		timeCountDown -= Time.deltaTime;
+	}
+
+	IEnumerator SpawnWave(){
+		waveNumber++;
+
+		for(int i=0;i < waveNumber;i++){
+			SpawnEnemy();
+			yield return new WaitForSeconds (0.5f);
+		}
+	}
+
+	void SpawnEnemy(){
+		Instantiate (enemy, SpawnPoint.position, SpawnPoint.rotation);
+	}
+}
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tower : MonoBehaviour {
-    public bool idle;
+	public static bool idle;
     private float speed = 15f;
 	// Use this for initialization
 	void Start () {
@@ -12,8 +12,11 @@ public class Tower : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (idle) {
-			transform.Rotate(0,0,speed*Time.deltaTime);
-        }
+		if (idle) { 
+			transform.Rotate (0, 0, speed * Time.deltaTime);
+		} else {
+			idle = false;
+		}
 	}
+		
 }
