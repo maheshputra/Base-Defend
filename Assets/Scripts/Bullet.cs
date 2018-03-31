3f06a22c89ce0df2429dc0966ede98875be3aec2
Kurou23 <32098313+Kurou23@users.noreply.github.com>
2018-03-31 14:05:50 +0700

    Adding Modified Map & Turret Shooting Script (with destroy enemy animation)

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	private Transform target;
+
+	public GameObject impactEffect;
+
+	public float bulletSpeed = 70f;
+
+	public void Seek (Transform _target){
+		target = _target;
+	} 
+
+	void Update(){
+		if (target == null) {
+			Destroy (gameObject);
+			return;
+		}
+
+		Vector3 dir = target.position - transform.position;
+		float distanceThisFrame = bulletSpeed * Time.deltaTime;
+
+		if(dir.magnitude <= distanceThisFrame){
+			HitTarget ();
+			return;
+		}
+
+		transform.Translate (dir.normalized * distanceThisFrame, Space.World);
+	}
+
+	public void HitTarget(){
+		GameObject effectIns = Instantiate (impactEffect, transform.position, transform.rotation);
+		Destroy (effectIns, 2f);
+
+		Destroy (target.gameObject);
+		Destroy (gameObject);
+	}
+}
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -3,20 +3,88 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Tower : MonoBehaviour {
-	public static bool idle;
-    private float speed = 15f;
+	
+	private Transform target;
+	[Header("Attributes")]
+	public float range = 7f;
+	public float fireRate = 1f;
+	public float fireCountdown = 0;
+
+	[Header("Unity Setup Fields")]
+	public float rotX = -90f;
+	public float rotY = 0f;
+
+	public string enemyTag = "enemy";
+
+	public bool idle;
+    //private float speed = 15f;
+
+	public Transform Head;
+
+	public GameObject bulletPrefabs;
+	public Transform firePoint;
+
+	public float turnSpeed = 10f;
+
 	// Use this for initialization
 	void Start () {
         idle = true;
+		InvokeRepeating ("UpdateTarget",0f,0.5f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (idle) { 
-			transform.Rotate (0, 0, speed * Time.deltaTime);
-		} else {
-			idle = false;
+		if(target == null){
+			Head.Rotate (0,0,turnSpeed * Time.deltaTime);
+			return;
 		}
+
+		Vector3 dir = target.position - transform.position;
+		Quaternion lookRotation = Quaternion.LookRotation (dir);
+		Vector3 rotation = Quaternion.Lerp(Head.rotation, lookRotation, turnSpeed * Time.deltaTime).eulerAngles;
+		Head.rotation = Quaternion.Euler (rotX,rotY,rotation.y);
+
+		if(fireCountdown <= 0f){
+			Shoot ();
+			fireCountdown = 1f / fireRate;
+		}
+
+		fireCountdown -= Time.deltaTime;
+
+	}
+
+	void Shoot(){
+		GameObject bulletGO = (GameObject)Instantiate(bulletPrefabs, firePoint.position, firePoint.rotation);
+		Bullet bullet = bulletGO.GetComponent<Bullet> ();
+
+		if(bullet != null){
+			bullet.Seek (target);
+		}
+	}
+
+	void UpdateTarget(){
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag (enemyTag);
+		float shotestDistance = Mathf.Infinity;
+		GameObject nearestEnemy = null;
+
+		foreach(GameObject enemy in enemies){
+			float distanceToEnemy = Vector3.Distance (transform.position, enemy.transform.position);
+			if (distanceToEnemy < shotestDistance) {
+				shotestDistance = distanceToEnemy;
+				nearestEnemy = enemy;
+			}
+
+			if (nearestEnemy != null && shotestDistance <= range) {
+				target = nearestEnemy.transform;
+			} else {
+				target = null;
+			}
+		}
+	}
+
+	void OnDrawGizmosSelected (){
+		Gizmos.color = Color.blue;
+		Gizmos.DrawWireSphere(transform.position, range);
 	}
 		
 }
